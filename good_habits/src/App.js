3b63754974ad4b7d0376c4f3f60262c6b6b30b70
govind <gs5818475>
2021-06-04 21:30:02 +0530

    update fetch fn

--- good_habits/src/App.js
+++ good_habits/src/App.js
@@ -5,18 +5,15 @@ import React from "react";
 const CLIENT_ID = '600958172796-05nnr8dkvl4h6u9hm4r6lc6slt3plhfh.apps.googleusercontent.com';
 
 function App() {
-  const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  
+  // React.useEffect(()=>{
+           
+  // })
 
   return (
     <div className="App">
       <header className="App-header">
-          <p>{!data ? "Loading...": data}</p>
       </header>
     </div>
   )
