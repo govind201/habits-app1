aa71634ddd7ed7e6d907696f548601952d55dd5a
govind <gs5818475>
2021-06-06 23:53:32 +0530

    add fish entity

--- server/models/fish.js
+++ server/models/fish.js
@@ -5,7 +5,8 @@ const AllFishSchema = new mongoose.Schema({
     type: String,
     price: Number,
     name: String,
+    isFed: Boolean,
 });
 
 // compile model from schema
-module.exports = mongoose.model("allfish", AllFishSchema);
+module.exports  = mongoose.model("allfish", AllFishSchema);
