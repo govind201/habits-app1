28053271b10d43682d3d010ecb5e4bc1dc7b0b49
govind <gs5818475>
2021-07-05 00:21:08 +0530

    add data files

--- server/app.js
+++ server/app.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-
-//Working with environment variables
 const dotenv = require('dotenv');
 dotenv.config();
-
-//include db related 
 const mongoose = require('mongoose');
 const Joi = require('joi');
-
-//import app, api, and socket 
-const api = require('./api/api');
 const auth = require('./auth');
 const socket  = require('./socket');
-
- 
+const fishHome = require('./routes/fishHome');
 const app = express();
-
-app.use(express.json()); //To process POST requests
+ 
 app.use(
   Session({
     secret: process.env.SESSION_KEY,
@@ -26,7 +17,6 @@ app.use(
     resave: false,
   })
 )
-const PORT = process.env.PORT || 3001;
 
 mongoose.connect( "mongodb://localhost:27017/ruth", {
   useNewUrlParser: true,
@@ -35,14 +25,26 @@ mongoose.connect( "mongodb://localhost:27017/ruth", {
   useCreateIndex: true
 }).then(console.log("Connected to ruth in mondodb")).catch(err => console.error("Could not connect ruth database", err))
 
+app.use(express.json()); //To process POST requests
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// add api to all routes
+app.use('/api/fishHome', fishHome);
+
+app.use(function (err, _req, res, _next) {
+  res.status(500).send('Internal Server Error', err);
+});
+appapp.use(function (err, _req, res, _next) {
+  res.status(500).send('Internal Server Error', err);
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=> {
     console.log(`Server lisening on port:${PORT}`);
 })
 
-console.log(process.env.DB_STR);
 
-app.get('*', (_, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+
+// app.get('*', (_, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
