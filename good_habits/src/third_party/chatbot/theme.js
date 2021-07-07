d6b05cc0f708898ee57596e7090b968f23211382
govind <gs5818475>
2021-07-08 01:57:13 +0530

    add thirdparty chatbot

---
+++ good_habits/src/third_party/chatbot/theme.js
@@ -0,0 +1,15 @@
+import rgba from "./common/rgba";
+
+
+export default {
+  // background: '#f5f8fb',
+  background: '#ffffff4d',
+  fontFamily: '\'Quicksand\', sans-serif',
+  headerBgColor: '#00CBD9',//'#6e48aa'
+  headerFontColor: '#fff',
+  headerFontSize: '16px',
+  botBubbleColor: '#00CBD9', //'#6E48AA'
+  botFontColor: '#fff',
+  userBubbleColor: '#F7CDDB',
+  userFontColor: '#4a4a4a'
+};
