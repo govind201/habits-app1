2f6badcf8aef37460ae4c3a18d0aa864c585d496
govind <gs5818475>
2021-07-18 09:10:00 +0530

    add navbar

---
+++ good_habits/src/components/modules/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {CLIENT_ID} from "../../utils/variables/CLIENT_ID";
+
+export default function Navbar() {
+    
+    return (
+        <div>
+            hellwo from Narabr
+        </div>
+    )
+}
