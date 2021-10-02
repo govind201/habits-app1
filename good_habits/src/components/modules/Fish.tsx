6b3276bdff98b852ac43a24671265281bc66e5ba
govind <gs5818475>
2021-10-03 01:17:06 +0530

    update app

--- good_habits/src/components/modules/Fish.tsx
+++ good_habits/src/components/modules/Fish.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {fishConstants} from "../../utils/variables"
 
 let horizontal = ['left', 'right'];
 let vertical = ['up', 'down'];
 
+const initialState = {
+    horizontal: Math.random() * 
+    (window.innerWidth - fishConstants.maxScaleFactor* fishConstants.imageWidth),
+  hDirection: horizontal[Math.floor(Math.random() * horizontal.length)],
+  hVelocity: 2,
+
+  vertical: Math.random() * 
+    (window.innerHeight -fishConstants.maxScaleFactor* fishConstants.imageHeight),
+  vDirection: vertical[Math.floor(Math.random() * vertical.length)],
+  vVelocity: 1,
+}
+
 
 export default function Fish() {
+    const [motion, setMotion] =  React.useState(initialState);
+
+    function chooseRandomMovement() {
+        let hVelocity = Math.random() *fishConstants.maxHorizontalVelocity;
+        let vVelocity = Math.random() * fishConstants.maxVerticalVelocity;
+        let hDirection = Math.random() < 0.5 ? 'right' : 'left';
+        let vDirection = Math.random() < 0.5 ? 'up' : 'down';
+
+       setMotion((prevState) =>  ({
+           ...prevState,
+           hVelocity,
+           vVelocity,
+           hDirection,
+           vDirection
+       }));
+
+       
+      }
 
     return (
         <div>
