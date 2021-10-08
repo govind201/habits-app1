db5f610e14b93f2eda8452d3e4dc8bf75dabbdd5
govind <gs5818475>
2021-10-09 01:44:18 +0530

    add typeorm

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+//const session = require('express-session');
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
