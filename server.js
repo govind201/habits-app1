9f5ee1a9560eec99f2f6096a4f4882a7869778ac
govind <gs5818475>
2022-03-31 21:39:52 +0530

    update default path resovler

--- server.js
+++ server.js
@@ -55,20 +55,21 @@ app.use(auth.populateCurrentUser);
 app.use("/api", api);
 
 // load the compiled react files, which will serve /index.html and /bundle.js
-// const reactPath = path.resolve(__dirname, "..", "client", "dist");
-// app.use(express.static(reactPath));
+const reactPath = path.resolve(__dirname, "..", "client", "public");
+app.use(express.static(reactPath));
 
 
 // app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
+// });
 
 // for all other routes, render index.html and let react router handle it
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(reactPath, "index.html"));
-// });
+app.get("*", (req, res) => {
+  // res.sendFile(path.join(reactPath, "index.html"));
+  res.send("InValid Path");
+});
 
 // any server errors cause this function to run
 app.use((err, req, res, next) => {
