abd20e868a29cd047c9b255e53537960140a718b
govind <gs5818475>
2022-04-01 12:20:56 +0530

    update for prod

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@
 
 const path = require("path");
 const entryFile = path.resolve(__dirname, "client", "src", "index.js");
-const outputDir = path.resolve(__dirname, "client", "public");
+const outputDir = path.resolve(__dirname, "client", "dist");
 
 const webpack = require("webpack");
 
